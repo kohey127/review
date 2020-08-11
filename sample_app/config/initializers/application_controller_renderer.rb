5395bd72f61a3f93a3961c45a74461459090449f
kohey127 <koooochoki@gmail.com>
2020-08-11 14:53:52 +0900

    1-F

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '501d5b45357ccd1db3ea7cbad47c99488ab87cb6ba4221fa086de59bbd6654e132b52d662450a84d6ae73e0f55cc406c44a3970172d357f29ed53456109781ed'

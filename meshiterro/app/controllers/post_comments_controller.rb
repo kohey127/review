87abf715b23385d23166c2e241543c1aaffc6e7a
kohey127 <koooochoki@gmail.com>
2020-08-18 13:47:38 +0900

    3-F(f)

--- meshiterro/app/controllers/post_comments_controller.rb
+++ meshiterro/app/controllers/post_comments_controller.rb
@@ -1,14 +1,17 @@
 class PostCommentsController < ApplicationController
   def create
-    comment = PostComment.new(post_comment_params)
-    comment.user_id = current_user.id
-    comment.post_image_id = comment.post_image.id
+    post_image = PostImage.find(params[:post_image_id])
+    comment = current_user.post_comments.new(post_comment_params)
+    # comment = PostComment.new(post_comment_params)
+    # comment.user_id = current_user.id
+    comment.post_image_id = post_image.id
     comment.save
     redirect_to post_image_path(post_image)
   end
 
   def destroy
-
+    PostComment.find_by(id: params[:id], post_image_id: params[:post_image_id]).destroy
+    redirect_to post_image_path(params[:post_image_id])
   end
 
   private
